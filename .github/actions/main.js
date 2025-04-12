91aa7b9864ad84511a529c383256ba0be9827f43
DICKSON39 <dicksonndumia19@gmail.com>
2025-04-12 22:26:58 +0300

    initial commit

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,20 +1,24 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-function run() {
-    //Get Inputs
-    const bucket = core.getInput('bucket', { required: true });
+async function run() {
+  try {
+    const bucket = core.getInput('bucket');
+    const distFolder = core.getInput('dist-folder');
+    const region = core.getInput('bucket-region');
 
-    const region = core.getInput('bucket-region', { required: true });
-    const distFolder = core.getInput('dist-folder', { required: true });
-    //Upload files
-    const s3URI = `s3://${bucket}`;
-    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${region}`)
-    `aws s3 folder1 s3://dicksonndumia --region us-east-1`;
-    //get url
-    const websiteURL = `http://${bucket}.s3-website-${region}.amazonaws.com`;
-    //http://dicksonndumia.s3-website-us-east-1.amazonaws.com
-    core.setOutput('website-url', websiteURL);
+   
+
+    // AWS CLI command: aws s3 sync
+    await exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${region} --delete`);
+
+    // Set the output for the GitHub Action
+    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-run()
+run();
