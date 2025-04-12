1b6a93e2b870d95d7c193536e7c13ba8c3b7de9e
DICKSON39 <dicksonndumia19@gmail.com>
2025-04-12 22:59:12 +0300

    initial commit

---
+++ Client/Jobsite/.github/actions/main.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+  try {
+    const bucket = core.getInput('bucket');
+    const distFolder = core.getInput('dist-folder');
+    const region = core.getInput('bucket-region');
+
+   
+
+    // AWS CLI command: aws s3 sync
+    await exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${region} --delete`);
+
+    // Set the output for the GitHub Action
+    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
